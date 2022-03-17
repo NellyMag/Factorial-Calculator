6c62e6e59d5d4f852859834ef3e03f56f9ee0a25
NellyMag <101749954+NellyMag@users.noreply.github.com>
2022-03-17 15:24:16 -0400

    This will calculate factorial of the entered number

---
+++ Factorial calculator app NM/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Factorial_calculator_app_NM.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="enternumber" runat="server" Text="Enter a nonnegative integer"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button" runat="server" OnClick="Button_Click" Text="Run Factorial" />
+        <br />
+        <asp:Label ID="factorial" runat="server" Text="The factorial of the entered number is"></asp:Label>
+&nbsp;&nbsp;
+        <asp:Label ID="outputlabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Factorial calculator app NM/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Factorial_calculator_app_NM
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            //Get user input
+            string value = TextBox1.Text;
+            //Conver string to int
+            int number = int.Parse(TextBox1.Text);
+            //Set up factorial value
+            double factorial = 1;
+            //multiply the factorial value
+            for (int count =1; count <= number; count++)
+            {
+                factorial = factorial * count;
+            }
+            outputlabel.Text = factorial.ToString();
+        }
+
+    }
+}
